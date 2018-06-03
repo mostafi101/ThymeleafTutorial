c162cdb54211927f80ff0befd047251881395cd8
mostafi <p.mostafij@gmail.com>
2018-06-03 00:33:16 -0700

    Initial commit

---
+++ src/main/java/com/blindcoder/thymeleaftutorials/ThymeleafTutorialsApplication.java
@@ -0,0 +1,12 @@
+package com.blindcoder.thymeleaftutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTutorialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafTutorialsApplication.class, args);
+    }
+}
